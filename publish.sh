d811ed55b3c41c9d2f7f95199cc7e9a7501a4351
simpleelegant <simpleelegant@163.com>
2024-07-27 12:25:48 +0800

    feat: publish to local path

---
+++ publish.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd packages
+for package in *
+do
+    cd $package
+    pnpm pack --pack-destination ../../dist
+    cd ../../dist
+    tar xf *.tgz
+    rm *.tgz
+    mv package $package
+    cd ../packages
+done
+
+
